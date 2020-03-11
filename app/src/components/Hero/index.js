9a89ac95257c846ffbfa9dd7bf5dd03182c322e7
Francisco Escobar Sabio <franciscoescobarsabio@gmail.com>
2020-03-11 16:15:39 -0300

    add search input

--- app/src/components/Hero/index.js
+++ app/src/components/Hero/index.js
@@ -1,12 +1,17 @@
 import React from "react";
-import { Wrapper } from "./styled";
+import { Wrapper, SearchInput } from "./styled";
 import { useSelector } from 'react-redux';
 const Hero = () => {
   const language = useSelector(state => state.languageReducer.language);
 
   return(
   <Wrapper>
+  <div>
     <h1>{language === "en-US" ? "The best premium photos of San Juan and Argentina." : "Las mejores fotos de San Juan y Argentina."}</h1>
+    <div>
+      <SearchInput placeholder="Search for premium San Juan Photos" />
+    </div>
+  </div>
   </Wrapper>
 )};
 
--- app/src/components/Hero/styled.js
+++ app/src/components/Hero/styled.js
@@ -1,9 +1,22 @@
 import styled from "styled-components";
-
+export const SearchInput = styled.input`
+  height: 52px;
+  border-radius: 5px;
+  border: none;
+  background: white;
+  width: 60%;
+  @media only screen and (max-width: 740px) {
+    height: 48px;
+  }
+  @media only screen and (max-width: 420px) {
+    height: 36px;
+  }
+`;
 export const Wrapper = styled.div`
   display: flex;
   width: 100%;
   height: 500px;
+  flex-wrap: wrap;
   align-items: center;
   justify-content: center;
   background-image: url("https://images.pexels.com/photos/3473569/pexels-photo-3473569.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
@@ -14,16 +27,29 @@ export const Wrapper = styled.div`
   @media only screen and (max-width: 420px) {
     height: 200px;
   }
-  > h1 {
+  > div > h1 {
     color: white;
     font-size: 48px;
     margin: 0 20%;
     @media only screen and (max-width: 740px) {
       font-size: 36px;
+      margin: 10% 10% 0 10%;
     }
     @media only screen and (max-width: 420px) {
       font-size: 24px;
-      margin: 0 10%;
+      margin: 7% 7% 0 7%;
+    }
+  }
+  > div > div {
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    height: 120px;
+    @media only screen and (max-width: 740px) {
+      height: 100px;
+    }
+    @media only screen and (max-width: 420px) {
+      height: 80px;
     }
   }
 `;
